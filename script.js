049c7ee3bf4c173fbaa4b5b1e0250a14cda8296c
mauriander <andermattenmauricio@gmail.com>
2023-06-02 22:43:46 -0300

    Se añadieron validaciones a los campos de formulario de compra

--- script.js
+++ script.js
@@ -17,29 +17,47 @@
   document.getElementById("cantidad").value = "";
     }
 
+  // VALIDACION
 
-// Menú wrapper
-// var menuWrapper = document.getElementById("menuWrapper");
-// var menuToggle = document.getElementById("menuToggle");
-// var wrapperMenu = document.getElementById("wrapperMenu");
-// var menu = document.getElementById("menu");
-// function toggleMenu() {
-//   wrapperMenu.classList.toggle("open");
-// }
+  //Acedo al id de mi boton en el que realizo los calculos y pongo un escuchador para cuando haga click
+  document.getElementById('resumenBtn').addEventListener('click', function() {
+    // Guardos en estas variables los el valor las palabras que las accedo a traves del id de cada input
+    var nombre = document.getElementById('enombre').value.trim();
+    var apellido = document.getElementById('eapellido').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var cantidad = document.getElementById('cantidad').value.trim();
+    //Esta linea es par avalidar el email la saque de stackvoerflow
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-// function checkWindowSize() {
-//   if (window.innerWidth < 1030) {
-//     menuWrapper.style.display = "block";
-//     menu.style.display = "none";
-//   } else {
-//     menu.style.display = "block";
-//     menuWrapper.style.display = "none";
-//     //wrapperMenu.classList.remove("open");
-//   }
-// }
 
-// menuToggle.addEventListener("click", toggleMenu);
-// window.addEventListener("resize", checkWindowSize);
+    //Voy preguntando por cada input si cumple las condiciones sino lanza un alert, que se ejecuta cuando presionamos el boton resumen para generar el calculo
+    if (nombre === ''|| nombre.length < 3 || nombre.length > 15) {
+      alert('Por favor, ingresa nombre Valido');
+      return;
+    }
+  
+    if (apellido === ''|| apellido.length < 3 || apellido.length > 25)  {
+      alert('Por favor, ingresa tu apellido Valido');
+      return;
+    }
+    
+    if (email === ''|| !emailRegex.test(email)) {
+      alert('Por favor, ingresa correo Valido');
+      return;
+    }
+  
+    if (cantidad === '') {
+      alert('Por favor, ingresa la cantidad.');
+      return;
+    }
+    document.getElementById('formulariocompra').submit();
+  });
+    
+ 
 
-// // Verificar el tamaño de la ventana al cargar la página
-// checkWindowSize();
+  
+
+
+
+
+
